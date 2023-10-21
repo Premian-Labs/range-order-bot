ee7c40cd0c4e6cba5cf5e485061e889d24c12682
froggiedev <froggie@premia.finance>
2023-10-20 14:49:05 -1000

    Replace abis shell script with package.json import

--- abis.sh
+++
@@ -1,6 +0,0 @@
-# HTTPs cloner with Personal Access Token auth scheme
-export GREEN='\033[0;32m'
-rm -rf ./abi && git clone https://github.com/Premian-Labs/v3-abi.git ./temp
-mkdir abi
-cd temp && mv -v ./abi/* ../abi && cd .. && rm -rf temp
-echo "${GREEN}ABIs cloned successfully"
