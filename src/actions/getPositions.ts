1c33290b66cb30f9dd127a019ad07c16df05ffb3
froggiedev <froggie@premia.finance>
2023-11-11 11:13:24 -1000

    Deconstruct the bird's nest

--- src/actions/getPositions.ts
+++ src/actions/getPositions.ts
@@ -1,4 +1,4 @@
-import { PoolKey, nextYearOfMaturities } from '@premia/v3-sdk'
+import { IPool, PoolKey, nextYearOfMaturities } from '@premia/v3-sdk'
 import { parseEther, formatEther } from 'ethers'
 import { marketParams } from '../config'
 import { lpAddress, addresses } from '../constants'
@@ -19,112 +19,9 @@ export async function getExistingPositions(market: string, spotPrice: number) {
 		)
 
 		await Promise.all(
-			maturities.map(async (maturityString) => {
-				let maturityTimestamp: number
-
-				try {
-					maturityTimestamp = createExpiration(maturityString)
-				} catch {
-					log.error(`Invalid maturity: ${maturityString}`)
-					return
-				}
-
-				await Promise.all(
-					[true, false].map(async (isCall) => {
-						const strikes = premia.options.getSuggestedStrikes(
-							parseEther(spotPrice.toString()),
-						)
-
-						await Promise.all(
-							strikes.map(async (strike) => {
-								const poolKey: PoolKey = {
-									base: marketParams[market].address,
-									quote: addresses.tokens.USDC,
-									oracleAdapter: addresses.core.ChainlinkAdapterProxy.address,
-									strike,
-									maturity: maturityTimestamp,
-									isCallPool: isCall,
-								}
-
-								log.debug(
-									`Checking: ${maturityString}-${formatEther(strike)}-${
-										isCall ? 'C' : 'P'
-									}`,
-								)
-
-								let poolAddress: string
-
-								try {
-									poolAddress = await premia.pools.getPoolAddress(poolKey)
-								} catch {
-									return
-								}
-
-								const pool = premia.contracts.getPoolContract(
-									poolAddress,
-									premia.multicallProvider as any,
-								)
-
-								let tokenIds: bigint[]
-
-								try {
-									tokenIds = (await pool.tokensByAccount(lpAddress)).filter(
-										(tokenId) => tokenId > 2n,
-									)
-								} catch {
-									return
-								}
-
-								if (tokenIds.length > 0) {
-									log.info(
-										`Existing positions for ${maturityString}-${strike}-${
-											isCall ? 'C' : 'P'
-										}: ${tokenIds.length}`,
-									)
-
-									log.debug(
-										`Existing positions token ids (${maturityString}-${strike}-${
-											isCall ? 'C' : 'P'
-										}): `,
-										tokenIds,
-									)
-								}
-
-								await Promise.all(
-									tokenIds.map(async (tokenId) => {
-										const positionKey = parseTokenId(tokenId)
-										const lpTokenBalance = parseFloat(
-											formatEther(await pool.balanceOf(lpAddress, tokenId)),
-										)
-
-										const position: Position = {
-											market,
-											posKey: {
-												owner: lpAddress,
-												operator: positionKey.operator,
-												lower: formatEther(positionKey.lower),
-												upper: formatEther(positionKey.upper),
-												orderType: positionKey.orderType,
-											},
-											depositSize: lpTokenBalance,
-											poolAddress,
-											strike: Number(formatEther(strike)),
-											maturity: maturityString,
-											isCall: isCall,
-										}
-
-										lpRangeOrders.push(position)
-									}),
-								)
-
-								lpRangeOrders = lpRangeOrders.filter(
-									(position) => position.depositSize > 0,
-								)
-							}),
-						)
-					}),
-				)
-			}),
+			maturities.map((maturityString) =>
+				processMaturity(maturityString, market, spotPrice, lpRangeOrders),
+			),
 		)
 
 		log.info(`Finished getting existing positions!`)
@@ -135,4 +32,170 @@ export async function getExistingPositions(market: string, spotPrice: number) {
 	}
 
 	return lpRangeOrders
+}
+
+async function processMaturity(
+	maturityString: string,
+	market: string,
+	spotPrice: number,
+	lpRangeOrders: Position[],
+) {
+	let maturityTimestamp: number
+
+	try {
+		maturityTimestamp = createExpiration(maturityString)
+	} catch {
+		log.error(`Invalid maturity: ${maturityString}`)
+		return
+	}
+
+	await Promise.all(
+		[true, false].map((isCall) =>
+			processOptionType(
+				isCall,
+				maturityString,
+				market,
+				spotPrice,
+				maturityTimestamp,
+				lpRangeOrders,
+			),
+		),
+	)
+}
+
+async function processOptionType(
+	isCall: boolean,
+	maturityString: string,
+	market: string,
+	spotPrice: number,
+	maturityTimestamp: number,
+	lpRangeOrders: Position[],
+) {
+	const strikes = premia.options.getSuggestedStrikes(
+		parseEther(spotPrice.toString()),
+	)
+
+	await Promise.all(
+		strikes.map((strike) =>
+			processStrike(
+				strike,
+				isCall,
+				maturityString,
+				market,
+				maturityTimestamp,
+				lpRangeOrders,
+			),
+		),
+	)
+}
+
+async function processStrike(
+	strike: bigint,
+	isCall: boolean,
+	maturityString: string,
+	market: string,
+	maturityTimestamp: number,
+	lpRangeOrders: Position[],
+) {
+	const poolKey: PoolKey = {
+		base: marketParams[market].address,
+		quote: addresses.tokens.USDC,
+		oracleAdapter: addresses.core.ChainlinkAdapterProxy.address,
+		strike,
+		maturity: maturityTimestamp,
+		isCallPool: isCall,
+	}
+
+	log.debug(
+		`Checking: ${maturityString}-${formatEther(strike)}-${isCall ? 'C' : 'P'}`,
+	)
+
+	let poolAddress: string
+
+	try {
+		poolAddress = await premia.pools.getPoolAddress(poolKey)
+	} catch {
+		return
+	}
+
+	const pool = premia.contracts.getPoolContract(
+		poolAddress,
+		premia.multicallProvider as any,
+	)
+
+	let tokenIds: bigint[]
+
+	try {
+		tokenIds = (await pool.tokensByAccount(lpAddress)).filter(
+			(tokenId) => tokenId > 2n,
+		)
+	} catch {
+		return
+	}
+
+	if (tokenIds.length > 0) {
+		log.info(
+			`Existing positions for ${maturityString}-${strike}-${
+				isCall ? 'C' : 'P'
+			}: ${tokenIds.length}`,
+		)
+
+		log.debug(
+			`Existing positions token ids (${maturityString}-${strike}-${
+				isCall ? 'C' : 'P'
+			}): `,
+			tokenIds,
+		)
+	}
+
+	await processTokenIds(
+		tokenIds,
+		pool,
+		maturityString,
+		strike,
+		isCall,
+		market,
+		poolAddress,
+		lpRangeOrders,
+	)
+}
+
+async function processTokenIds(
+	tokenIds: bigint[],
+	pool: IPool,
+	maturityString: string,
+	strike: bigint,
+	isCall: boolean,
+	market: string,
+	poolAddress: string,
+	lpRangeOrders: Position[],
+) {
+	await Promise.all(
+		tokenIds.map(async (tokenId) => {
+			const positionKey = parseTokenId(tokenId)
+			const lpTokenBalance = parseFloat(
+				formatEther(await pool.balanceOf(lpAddress, tokenId)),
+			)
+
+			if (lpTokenBalance > 0) {
+				const position: Position = {
+					market,
+					posKey: {
+						owner: lpAddress,
+						operator: positionKey.operator,
+						lower: formatEther(positionKey.lower),
+						upper: formatEther(positionKey.upper),
+						orderType: positionKey.orderType,
+					},
+					depositSize: lpTokenBalance,
+					poolAddress,
+					strike: Number(formatEther(strike)),
+					maturity: maturityString,
+					isCall: isCall,
+				}
+
+				lpRangeOrders.push(position)
+			}
+		}),
+	)
 }
