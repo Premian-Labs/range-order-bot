1f8b972735b41016d83f8da6028b4fe3e5ee5c05
Froggie <froggie@premia.finance>
2023-11-28 12:22:25 -1000

    Formatting

--- src/actions/hydrateStrikes.ts
+++ src/actions/hydrateStrikes.ts
@@ -5,12 +5,13 @@ import {
 import { marketParams } from '../config'
 import { createExpiration, getTTM } from '../utils/dates'
 import { log } from '../utils/logs'
-/*
-NOTE: only runs if strike arrays are NOT populated AND we have a spot price for the market
-Since the suggested strikes is very broad, we narrow it down by min/max delta to more efficient
-downstream processing and error/warning messages.
 
-IMPORTANT: we filter by the delta by the FURTHEST exp (the broadest possible strike filter)
+/*
+	NOTE: only runs if strike arrays are NOT populated AND we have a spot price for the market
+	Since the suggested strikes is very broad, we narrow it down by min/max delta to more efficient
+	downstream processing and error/warning messages.
+	
+	IMPORTANT: we filter by the delta by the FURTHEST exp (the broadest possible strike filter)
  */
 export async function hydrateStrikes(market: string, spotPrice: number) {
 	if (!spotPrice) return
